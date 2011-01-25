d35c3982d0c1ac3c900f02c2606269f45237dbd6
Ilya 'FileJunkie' Ershov <filejunkie@gmail.com>
2011-01-25 16:46:52 +0300

    Basic info retrieving from Russian Post

---
+++ trackerbot.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use HTTP::Request::Common qw(POST);
+use LWP::UserAgent;
+
+# Tracking numbers
+my @tracks = ("CJ216818892US");
+
+sub parse_russian_post{
+	my $track = $_[0];
+	
+	my $ua = LWP::UserAgent->new;
+
+	my $req = POST 'http://info.russianpost.ru/servlet/post_item',
+		[ action => 'search', searchType => 'barCode', show_form => 'yes', barCode => $track ];
+
+	print $ua->request($req)->as_string;
+}
+
+foreach my $track (@tracks){
+	parse_russian_post($track);
+}
