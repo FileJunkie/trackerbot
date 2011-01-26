ece069d802234d670ae4efda718078c7038bd701
Ilya 'FileJunkie' Ershov <filejunkie@gmail.com>
2011-01-26 10:41:03 +0300

    Basic error checking

--- trackerbot.pl
+++ trackerbot.pl
@@ -4,21 +4,35 @@ use strict;
 use warnings;
 
 use Encode;
+use HTML::TreeBuilder;
 use HTTP::Request::Common qw(POST);
 use LWP::UserAgent;
 
 # Tracking numbers
 my @tracks = ("CJ216818892US");
 
+binmode STDOUT, ":utf8";
+
 sub parse_russian_post{
 	my $track = $_[0];
 	
 	my $ua = LWP::UserAgent->new;
-
 	my $req = POST 'http://info.russianpost.ru/servlet/post_item',
 		[ action => 'search', searchType => 'barCode', show_form => 'yes', barCode => $track ];
 
-	print decode("CP1251", $ua->request($req)->as_string);
+	my $tree = HTML::TreeBuilder->new;
+	$tree->parse(decode("CP1251", $ua->request($req)->as_string));
+	
+	my @header = $tree->look_down("_tag", "p", "class", "page_TITLE");
+	my $header_text = $header[1]->as_text;
+	if($header_text !~ /РЕЗУЛЬТАТЫ ПОИСКА/){
+		print "Ok\n";
+	}
+	else{
+		print "Fail\n";
+	}
+	
+	$tree->delete;
 }
 
 foreach my $track (@tracks){
