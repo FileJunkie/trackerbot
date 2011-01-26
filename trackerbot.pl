f6e1bbd1bed8dd4ccb1545112bf327fd17d8f938
Ilya 'FileJunkie' Ershov <filejunkie@gmail.com>
2011-01-26 11:37:51 +0300

    Data parsing finished

--- trackerbot.pl
+++ trackerbot.pl
@@ -25,14 +25,39 @@ sub parse_russian_post{
 	$tree->parse(decode("CP1251", $ua->request($req)->as_string));
 	
 	my @header = $tree->look_down("_tag", "p", "class", "page_TITLE");
+	# checking if tracking # is OK
 	if(!defined $header[1]){
-		print "Error parsing page: second header not found.\n";
-		return;
+		print "$track Error parsing page: second header not found.\n";
+		$tree->delete;
+		return undef;
 	}
 	my $header_text = $header[1]->as_text;
 
+	# checking if tracking # is OK
 	if($header_text !~ /РЕЗУЛЬТАТЫ ПОИСКА/){
-		print "Error parsing page: incorrent heading content. Maybe incorrect tracking number?\n";
+		print "$track Error parsing page: incorrent heading content. Maybe incorrect tracking number?\n";
+		$tree->delete;
+		return undef;
+	}
+	
+	# looking for results table
+	my @tables = $tree->look_down("_tag", "table", "width", "");
+	if(@tables != 1){
+		print "$track Error parsing page: too much tables found.\n";
+		$tree->delete;
+		return undef;
+	}
+	
+	# extracting data rows
+	my @rows = $tables[0]->look_down("_tag", "tr", sub {
+		$_[0]->attr('class') =~ /row_[01]_light/;
+	});
+	
+	# extracting data
+	my @data = ();
+	foreach my $row (@rows){
+		my @cells = $row->look_down("_tag", "td");
+		$data[@data] = $cells[1]->as_text . "\t" . $cells[0]->as_text . "\t" . $cells[3]->as_text;
 	}
 	
 	$tree->delete;
